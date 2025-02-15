4534fe5c1d9f963b21cbeb0636208afed989e0d5
wandi1209 <wandider@gmail.com>
2025-02-15 18:35:22 +0700

    writing to a file through FS module

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/cars", (req, res) => {
-  res.send("carss here");
+app.get("/write", (req, res) => {
+  fs.appendFile("./data.txt", "\nCherry", (err) => {
+    if (err) {
+      res.send("There was an error writen the file!");
+    }
+    res.send("Data writen successfully");
+  });
 });
 
 app.listen(8000, () => {
