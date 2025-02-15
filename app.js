c095bc9ca1cd187f355ab51ec640702e15ea7d42
wandi1209 <wandider@gmail.com>
2025-02-15 13:06:26 +0700

    using filesystem

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello from the server");
+  // read file here
+  fs.readFile("./data.txt", "utf-8", (err, data) => {
+    if (err) {
+      res.send("There was an error accessiing the file!");
+    }
+    res.send(data);
+  });
 });
 
 app.get("/cars", (req, res) => {
