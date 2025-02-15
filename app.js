89ae66e4f0ade298a5a79ab178b527d66a1f36df
wandi1209 <wandider@gmail.com>
2025-02-15 12:48:02 +0700

    using express for creating server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello from the server");
+});
+
+app.get("/cars", (req, res) => {
+  res.send("carss");
+});
+
+app.listen(8000, () => {
+  console.log("server started");
+});
